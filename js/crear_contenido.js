8b396bf92ea7a09a7fc7a3831c5fa6100736f070
Quiroga10 <juniorquirogamio@gmail.com>
2023-04-05 15:27:10 -0400

    orden pedido js

---
+++ js/crear_contenido.js
@@ -0,0 +1,24 @@
+function pintarOrden() {
+    const tbody = document.querySelector('table tbody');
+    limpiarHTML(tbody)
+    productosSeleccionados.forEach(producto => {
+        const tr = document.createElement('tr');
+        const tdName = document.createElement('td');
+        const tdPrice = document.createElement('td');
+        const tdQty = document.createElement('td');
+        tdQty.classList.add("cantidadInput");
+        const inputQty = document.createElement("input");
+        inputQty.type = 'text';
+        inputQty.classList.add('form-control', "cantidadInput");
+        inputQty.value = producto.cantidadProducto;
+        inputQty.disabled = true;
+        tdQty.append(inputQty);
+        tdName.textContent = producto.nombreProducto;
+        tdPrice.textContent = producto.precioProducto;
+        tr.append(tdName);
+        tr.append(tdPrice);
+        tr.append(tdQty);
+        tbody.append(tr);
+    });
+    //generarOrden();
+}

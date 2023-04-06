b1788690fe3da2e8ac811a50121c932aa7634f99
Quiroga10 <juniorquirogamio@gmail.com>
2023-04-05 21:36:36 -0400

    agregando cantidad productos

--- js/interaccion_carrito.js
+++ js/interaccion_carrito.js
@@ -3,35 +3,70 @@ agregarAlCarritoBotones.forEach(boton => {
     boton.addEventListener('click', agregarAlCarrito);
 });
 
+const carrito = [];
+
 function agregarAlCarrito(evento) {
     const boton = evento.target;
     const item = boton.parentElement;
     const producto = item.dataset.producto;
-    const precio = item.dataset.precio;
+    const precio = parseFloat(item.dataset.precio);
     agregarItemAlCarrito(producto, precio);
     actualizarCarrito();
 }
 
 function agregarItemAlCarrito(producto, precio) {
-    const itemCarrito = document.createElement('li');
-    itemCarrito.classList.add('cart-item');
-    const itemCarritoContenido = `
-        <span class="cart-item-title">${producto}</span>
-        <span class="cart-item-price">$${precio}</span>
-    `;
-    itemCarrito.innerHTML = itemCarritoContenido;
-    const carrito = document.querySelector('.cart-items');
-    carrito.appendChild(itemCarrito);
+    for (let i = 0; i < carrito.length; i++) {
+        const item = carrito[i];
+        if (item.producto === producto) {
+            item.cantidad++;
+            return;
+        }
+    }
+    carrito.push({ producto, precio, cantidad: 1 });
 }
 
 function actualizarCarrito() {
-    const carritoItems = document.querySelectorAll('.cart-item');
-    const totalCarrito = document.querySelector('.cart-total');
+    const carritoItems = document.querySelector('.cart-items');
+    carritoItems.innerHTML = '';
+
     let total = 0;
-    carritoItems.forEach(item => {
-        const precioElemento = item.querySelector('.cart-item-price');
-        const precio = Number(precioElemento.textContent.replace('$', ''));
-        total = total + precio;
-    });
+    for (let i = 0; i < carrito.length; i++) {
+        const item = carrito[i];
+
+        const itemCarrito = document.createElement('li');
+        itemCarrito.classList.add('cart-item');
+        const itemCarritoContenido = `
+            <span class="cart-item-title">${item.producto}</span>
+            <span class="cart-item-price">$${item.precio.toFixed(2)}</span>
+            <span class="cart-item-quantity">${item.cantidad}</span>
+            <i class="fa fa-times-circle"></i>
+        `;
+        itemCarrito.innerHTML = itemCarritoContenido;
+        carritoItems.appendChild(itemCarrito);
+
+        total += item.precio * item.cantidad;
+
+        const eliminarProducto = itemCarrito.querySelector('.fa-times-circle');
+        eliminarProducto.addEventListener('click', () => {
+            eliminarItemDelCarrito(i);
+            actualizarCarrito();
+        });
+    }
+    const totalCarrito = document.querySelector('.cart-total');
     totalCarrito.textContent = total.toFixed(2);
+
+    const botonesEliminar = document.querySelectorAll('.fa-times-circle');
+    if (carrito.length > 0) {
+        botonesEliminar.forEach(boton => {
+            boton.style.display = 'inline-block';
+        });
+    } else {
+        botonesEliminar.forEach(boton => {
+            boton.style.display = 'none';
+        });
+    }
+}
+
+function eliminarItemDelCarrito(index) {
+    carrito.splice(index, 1);
 }
